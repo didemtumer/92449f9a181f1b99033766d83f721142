f2dc1be97c5e1f01589771d069603980478f30a7
Didem Tumer <didemdizman@gmail.com>
2021-06-24 18:09:43 +0300

    V-1
    First Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-toolbar flat color="primary">
+      <v-toolbar-title>Otel</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn height="20">
+          Yeni Rezervasyon Yap
+        </v-btn>
+
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+
+    data (){
+      return{
+        
+      }
+    }
+  }
+</script>
