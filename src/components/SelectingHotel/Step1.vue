a6dc75fcbe17d12b0328f53315dfe6262da26d01
Didem Tumer <didemdizman@gmail.com>
2021-06-26 13:14:11 +0300

    V-1
    Adding date-picker to step1 component

--- src/components/SelectingHotel/Step1.vue
+++ src/components/SelectingHotel/Step1.vue
@@ -3,23 +3,31 @@
     <v-container>
       <v-row>
         <v-col cols="12" style="background-color: #e9ecef">
-          <v-select dense solo></v-select>
+          <v-select
+            v-model="hotelItem"
+            dense
+            solo
+            label="Rezervasyon Yapmak İstediğiniz Oteli Seçiniz"
+            :items="hotelName"
+          >
+            <v-icon>mdi-home-search-outline</v-icon>
+          </v-select>
         </v-col>
       </v-row>
       <v-row>
+        <!-- Check-in -->
         <v-col cols="12" sm="6" md="3">
           <v-menu
-            ref="menu"
             v-model="menu"
             :close-on-content-click="false"
-            :return-value.sync="date"
+            :nudge-right="40"
             transition="scale-transition"
             offset-y
             min-width="auto"
           >
             <template v-slot:activator="{ on, attrs }">
               <v-text-field
-                v-model="date"
+                v-model="checkIn"
                 label="Check-in"
                 prepend-icon="mdi-calendar"
                 readonly
@@ -27,31 +35,25 @@
                 v-on="on"
               ></v-text-field>
             </template>
-            <v-date-picker v-model="date" no-title scrollable>
-              <v-spacer></v-spacer>
-              <v-btn text color="primary" @click="menu = false">
-                Cancel
-              </v-btn>
-              <v-btn text color="primary" @click="$refs.menu.save(date)">
-                OK
-              </v-btn>
-            </v-date-picker>
+            <v-date-picker
+              v-model="checkIn"
+              @input="menu = false"
+            ></v-date-picker>
           </v-menu>
         </v-col>
-        <v-spacer></v-spacer>
+        <!-- Check-out -->
         <v-col cols="12" sm="6" md="3">
           <v-menu
-            ref="menu"
-            v-model="menu"
+            v-model="menu2"
             :close-on-content-click="false"
-            :return-value.sync="date"
+            :nudge-right="40"
             transition="scale-transition"
             offset-y
             min-width="auto"
           >
             <template v-slot:activator="{ on, attrs }">
               <v-text-field
-                v-model="date2"
+                v-model="checkOut"
                 label="Check-out"
                 prepend-icon="mdi-calendar"
                 readonly
@@ -59,43 +61,62 @@
                 v-on="on"
               ></v-text-field>
             </template>
-            <v-date-picker v-model="date" no-title scrollable>
-              <v-spacer></v-spacer>
-              <v-btn text color="primary" @click="menu = false">
-                Cancel
-              </v-btn>
-              <v-btn text color="primary" @click="$refs.menu.save(date)">
-                OK
-              </v-btn>
-            </v-date-picker>
+            <v-date-picker
+              v-model="checkOut"
+              @input="menu2 = false"
+            ></v-date-picker>
           </v-menu>
         </v-col>
-        <v-spacer></v-spacer>
         <v-col cols="12" sm="6" md="3" class="mt-4">
-          <v-select label="Yetişkin Sayısı" :items="adults" dense> </v-select>
+          <v-select
+            label="Yetişkin Sayısı"
+            v-model="adultCount"
+            :items="adults"
+            dense
+          >
+          </v-select>
         </v-col>
         <v-col cols="12" sm="6" md="3" class="mt-4">
-          <v-select label="Çocuk Sayısı" :items="childeren" dense> </v-select>
+          <v-select
+            label="Çocuk Sayısı"
+            v-model="childCount"
+            :items="children"
+            dense
+          >
+          </v-select>
         </v-col>
       </v-row>
     </v-container>
   </v-layout>
 </template>
 <script>
+import { mapActions, mapGetters } from "vuex";
 export default {
   data() {
     return {
-      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-        .toISOString()
-        .substr(0, 10),
-      date2: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-        .toISOString()
-        .substr(0, 10),
       menu: false,
+      menu2: false,
+      hotelItem: "",
+      checkIn: new Date().toISOString().substr(0, 10),
+      checkOut: new Date().toISOString().substr(0, 10),
+      childCount: null,
+      adultCount: null,
       adults: [1, 2, 3, 4, 5],
-      childeren: [1, 2, 3, 4, 5],
+      children: [1, 2, 3, 4, 5],
     };
   },
-  computed: {},
+  computed: {
+    ...mapGetters(["getHotelList", "getHotelDetails"]),
+    hotelName() {
+      return this.getHotelList.map((item) => item.hotel_name);
+    },
+  },
+  methods: {
+    ...mapActions(["loadHotelList", "loadHotelDetails"]),
+  },
+  created() {
+    this.loadHotelList();
+    this.loadHotelDetails();
+  },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,52 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    hotelList: [],
+    hotelDetails: []
   },
   mutations: {
+    setHotelList(state, payload) {
+      state.hotelList = payload;
+    },
+    setHotelDetails(state, payload) {
+      state.hotelDetails = payload;
+      console.log(payload);
+    },
   },
   actions: {
+    loadHotelList({ commit }) {
+      axios.get(`https://5f6d939160cf97001641b049.mockapi.io/tkn/hotels`).then((response) => {
+        console.log(response.data);
+        let hotelList = response.data;
+        commit("setHotelList", hotelList);
+      })
+        .catch((e) => {
+          console.log("error", e);
+        })
+    },
+    loadHotelDetails({ commit }) {
+      axios.get(`https://5f6d939160cf97001641b049.mockapi.io/tkn/hotel-details`).then((response) => {
+        console.log("Otel Detayları", response.data);
+        let hotelDetails = response.data;
+        commit("setHotelDetails", hotelDetails);
+      })
+        .catch((e) => {
+          console.log("error", e);
+        })
+    },
   },
-  modules: {
+  getters: {
+    getHotelList(state) {
+      return state.hotelList;
+    },
+    getHotelDetails(state) {
+      return state.hotelDetails;
+    }
   }
+
 })
